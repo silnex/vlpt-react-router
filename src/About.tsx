e86b72874178b1bcf3b43b38ada4f27ad3ac76bf
silnex <silnex@silnex.kr>
2022-12-21 23:29:23 +0900

    Add search params

--- src/About.tsx
+++ src/About.tsx
@@ -1,8 +1,14 @@
+import { useSearchParams } from "react-router-dom";
+
 const About = () => {
+  const [searchParams] = useSearchParams();
+  const detail = searchParams.get("detail") ?? false;
+
   return (
     <div>
       <h1>소개</h1>
-      <p>이곳은 소개페이지</p>
+      <p>이곳은 소개페이지 입니다.</p>
+      {detail && <p>추가적인 정보가 어쩌고 저쩌고..</p>}
     </div>
   );
 };
